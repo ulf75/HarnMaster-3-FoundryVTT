f5d69575e5e4b788f5d484259ad1d821ba41ae67
Ulf Willers <ulf@cad-ray.com>
2025-03-17 20:14:55 +0100

    fix

--- module/condition/shocked.js
+++ module/condition/shocked.js
@@ -1,21 +1,17 @@
 const SHOCKED_ICON = 'systems/hm3/images/icons/svg/shock.svg';
 const INDEFINITE = Number.MAX_SAFE_INTEGER;
 
-const ON_CREATE_MACRO = (token) => {
-    `
+const ON_CREATE_MACRO = (token) => `
 const token = canvas.tokens.get('${token.id}');
 const unconscious = token.hasCondition(game.hm3.enums.Condition.UNCONSCIOUS);
 if (!unconscious) await game.hm3.GmSays("<b>" + token.name + "</b> is in <b>Shock</b> and displays a variety of symptoms including pallor, cold sweats, weakness, nausea, thirst, and groaning. <b>" + token.name + "</b> is incoherent and gazes helplessly at the injuries.", "Combat 14");
 `;
-};
 
-const ON_TURN_START_MACRO = (token) => {
-    return `
+const ON_TURN_START_MACRO = (token) => `
 const token = canvas.tokens.get('${token.id}');
 const unconscious = token.hasCondition(game.hm3.enums.Condition.UNCONSCIOUS);
 if (!unconscious) await game.hm3.GmSays("<b>" + token.name + "</b> is in <b>Shock</b>. Shock prevents the use of skills, spells, and psionic talents. In a combat situation, a character in shock may Rest, Walk/Crawl (at half move), or be led away; the character will <b>Ignore</b> any attacks.", "Combat 18");
 `;
-};
 
 /**
  *
