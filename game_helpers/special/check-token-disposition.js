e13bebb775ba2bf43633e1a51f753cf238f3204e
Ulf Willers <ulf@cad-ray.com>
2025-05-14 14:58:04 +0200

    debug

--- game_helpers/special/check-token-disposition.js
+++ game_helpers/special/check-token-disposition.js
@@ -1,10 +1,14 @@
 // Hook: On token's turn start, if disposition is neutral/secret, ask to set friendly or hostile.
 
+console.debug('HM3 | Check Token Disposition', triggerArgs);
+
 const combatant = triggerArgs[0].combatant;
 if (!combatant?.tokenId || combatant.hidden) return;
 
 const token = canvas.tokens.get(combatant.tokenId);
 if (!token || !token.document) return;
+
+console.debug('HM3 | Check Token Disposition: ' + combatant.name);
 
 const currentDisposition = token.document.disposition;
 if (currentDisposition === CONST.TOKEN_DISPOSITIONS.NEUTRAL || currentDisposition === CONST.TOKEN_DISPOSITIONS.SECRET) {
