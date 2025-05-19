b6dfdaf65776b8716f9db43cd267eae57cf43522
Ulf Willers <ulf@cad-ray.com>
2025-05-19 19:26:06 +0200

    refactored

--- module/hm3-combatant.js
+++ module/hm3-combatant.js
@@ -4,16 +4,24 @@ export class HarnMasterCombatant extends Combatant {
      * @override
      */
     getInitiativeRoll(formula) {
+        const mark = game.settings.get('hm3', 'autoMarkUsedSkills');
         if (this.actor.system.mounted) {
             // For mounted combat, initiative is equal to Riding EML (COMBAT 20)
-            const iniSkill = this.actor.items.find((item) => item.type === game.hm3.ItemType.SKILL && item.name.includes('Riding')).system;
-            this.actor.system.initiative = iniSkill.effectiveMasteryLevel;
-            this.actor.system.initiative += iniSkill.skillBase.value / 10;
+            const iniSkill = this.actor.items.find((item) => item.type === game.hm3.ItemType.SKILL && item.name.includes('Riding'));
+            this.actor.system.initiative = iniSkill.system.effectiveMasteryLevel;
+            this.actor.system.initiative += iniSkill.system.skillBase.value / 10;
+            if (mark && !iniSkill.system.improveFlag) {
+                iniSkill.update({'system.improveFlag': true});
+            }
         } else {
-            const iniSkill = this.actor.items.find((item) => item.type === game.hm3.ItemType.SKILL && item.name === 'Initiative').system;
-            this.actor.system.initiative = !this.token?.hasCondition(game.hm3.Condition.SHOCKED) ? iniSkill.effectiveMasteryLevel : 0;
-            this.actor.system.initiative += iniSkill.skillBase.value / 10;
+            const iniSkill = this.actor.items.find((item) => item.type === game.hm3.ItemType.SKILL && item.name === 'Initiative');
+            this.actor.system.initiative = !this.token?.hasCondition(game.hm3.Condition.SHOCKED) ? iniSkill.system.effectiveMasteryLevel : 0;
+            this.actor.system.initiative += iniSkill.system.skillBase.value / 10;
+            if (mark && !iniSkill.system.improveFlag) {
+                iniSkill.update({'system.improveFlag': true});
+            }
         }
+
         return super.getInitiativeRoll(formula);
     }
 }
