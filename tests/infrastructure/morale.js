a40202529a5fd07b1d03855e677cb15bd5583f2b
Ulf Willers <ulf@cad-ray.com>
2025-05-27 18:08:29 +0200

    refactored

--- tests/infrastructure/morale.js
+++ tests/infrastructure/morale.js
@@ -1,37 +1,37 @@
 export class TestCase extends game.hm3.BaseTest {
-    CENTER = {x: 7870, y: 14258};
-
     async _test() {
-        const token = await this._dropActor(this.actors.get('Alice'), this.CENTER);
+        const Center = {x: 7870, y: 14258};
+        const Condition = game.hm3.Condition;
+        const token = await this._dropActor(this.actors.get('Alice'), Center);
 
-        const berserk = await token.addCondition(game.hm3.Condition.BERSERK);
-        console.info('Adding condition: %s to token: %s', game.hm3.Condition.BERSERK, token.name);
+        const berserk = await token.addCondition(Condition.BERSERK);
+        console.info('Adding condition: %s to token: %s', Condition.BERSERK, token.name);
         console.assert(berserk && berserk instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', berserk);
 
-        const broken = await token.addCondition(game.hm3.Condition.BROKEN);
-        console.info('Adding condition: %s to token: %s', game.hm3.Condition.BROKEN, token.name);
+        const broken = await token.addCondition(Condition.BROKEN);
+        console.info('Adding condition: %s to token: %s', Condition.BROKEN, token.name);
         console.assert(broken && broken instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', broken);
-        console.assert(!token.hasCondition(game.hm3.Condition.BERSERK), 'Token DOES have condition: %s', game.hm3.Condition.BERSERK);
+        console.assert(!token.hasCondition(Condition.BERSERK), 'Token DOES have condition: %s', Condition.BERSERK);
 
-        const c = await token.addCondition(game.hm3.Condition.CAUTIOUS);
-        console.info('Adding condition: %s to token: %s', game.hm3.Condition.CAUTIOUS, token.name);
-        console.assert(c && c instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', c);
-        console.assert(!token.hasCondition(game.hm3.Condition.BROKEN), 'Token DOES have condition: %s', game.hm3.Condition.BROKEN);
+        const cautious = await token.addCondition(Condition.CAUTIOUS);
+        console.info('Adding condition: %s to token: %s', Condition.CAUTIOUS, token.name);
+        console.assert(cautious && cautious instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', cautious);
+        console.assert(!token.hasCondition(Condition.BROKEN), 'Token DOES have condition: %s', Condition.BROKEN);
 
-        const d = await token.addCondition(game.hm3.Condition.DESPERATE);
-        console.info('Adding condition: %s to token: %s', game.hm3.Condition.DESPERATE, token.name);
-        console.assert(d && d instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', d);
-        console.assert(!token.hasCondition(game.hm3.Condition.CAUTIOUS), 'Token DOES have condition: %s', game.hm3.Condition.CAUTIOUS);
+        const desperate = await token.addCondition(Condition.DESPERATE);
+        console.info('Adding condition: %s to token: %s', Condition.DESPERATE, token.name);
+        console.assert(desperate && desperate instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', desperate);
+        console.assert(!token.hasCondition(Condition.CAUTIOUS), 'Token DOES have condition: %s', Condition.CAUTIOUS);
 
-        const e = await token.addCondition(game.hm3.Condition.EMPOWERED);
-        console.info('Adding condition: %s to token: %s', game.hm3.Condition.EMPOWERED, token.name);
-        console.assert(e && e instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', e);
-        console.assert(!token.hasCondition(game.hm3.Condition.DESPERATE), 'Token DOES have condition: %s', game.hm3.Condition.DESPERATE);
+        const empowered = await token.addCondition(Condition.EMPOWERED);
+        console.info('Adding condition: %s to token: %s', Condition.EMPOWERED, token.name);
+        console.assert(empowered && empowered instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', empowered);
+        console.assert(!token.hasCondition(Condition.DESPERATE), 'Token DOES have condition: %s', Condition.DESPERATE);
 
-        const w = await token.addCondition(game.hm3.Condition.WEAKENED);
-        console.info('Adding condition: %s to token: %s', game.hm3.Condition.WEAKENED, token.name);
-        console.assert(w && w instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', w);
-        console.assert(!token.hasCondition(game.hm3.Condition.EMPOWERED), 'Token DOES have condition: %s', game.hm3.Condition.EMPOWERED);
+        const weakened = await token.addCondition(Condition.WEAKENED);
+        console.info('Adding condition: %s to token: %s', Condition.WEAKENED, token.name);
+        console.assert(weakened && weakened instanceof ActiveEffect, 'Condition is NOT an ActiveEffect: %O', weakened);
+        console.assert(!token.hasCondition(Condition.EMPOWERED), 'Token DOES have condition: %s', Condition.EMPOWERED);
 
         console.assert(game.hm3.resolveMap.size === 0, 'Resolve map is NOT empty: %O', game.hm3.resolveMap);
     }
