69421b0328a4f8d4bd6bf98603be1b2d556a79f3
Ulf Willers <ulf@cad-ray.com>
2025-09-15 14:30:57 +0200

    config

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,5 +4,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    output: {
+        filename: 'hm3.min.js'
+    }
 });
