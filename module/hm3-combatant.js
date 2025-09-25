c755a451383dba2cfe507354dae1b49c8ff1da7c
Ulf Willers <ulf@cad-ray.com>
2025-09-25 11:42:56 +0200

    ini

--- module/hm3-combatant.js
+++ module/hm3-combatant.js
@@ -1,4 +1,7 @@
-import {ItemType} from './hm3-types';
+// @ts-check
+
+import {Condition} from './hm3-types';
+import {truncate} from './utility';
 
 export class CombatantHM3 extends Combatant {
     /**
@@ -6,25 +9,23 @@ export class CombatantHM3 extends Combatant {
      * @override
      */
     getInitiativeRoll(formula) {
+        const start = performance.now();
+
         const mark = game.settings?.get('hm3', 'autoMarkUsedSkills');
-        if (this.actor.system.mounted) {
+        if (this.actor?.system.mounted) {
             // For mounted combat, initiative is equal to Riding EML (COMBAT 20)
-            const iniSkill = this.actor.items.find(
-                (item) => item.type === ItemType.SKILL && item.name.includes('Riding')
-            );
-            this.actor.system.initiative = iniSkill.system.effectiveMasteryLevel;
-            this.actor.system.initiative += iniSkill.system.skillBase.value / 10;
-            if (mark) iniSkill.update({'system.improveFlag': iniSkill.system.improveFlag + 1});
-        } else {
-            const iniSkill = this.actor.items.find(
-                (item) => item.type === ItemType.SKILL && item.name === 'Initiative'
-            );
-            this.actor.system.initiative = !this.token?.hasCondition(hm3.Condition.SHOCKED)
-                ? iniSkill.system.effectiveMasteryLevel
-                : 0;
-            this.actor.system.initiative += iniSkill.system.skillBase.value / 10;
-            if (mark) iniSkill.update({'system.improveFlag': iniSkill.system.improveFlag + 1});
+            const iniSkill = this.actor.proxy.Skill('Riding');
+            this.actor.system.initiative = !this.token?.hasCondition(Condition.SHOCKED) ? iniSkill?.EML : 5;
+            this.actor.system.initiative += iniSkill?.SB.value / 10;
+            if (mark) iniSkill?.item.update({'system.improveFlag': iniSkill.improveFlag + 1});
+        } else if (this.actor) {
+            const iniSkill = this.actor.proxy.Skill('Initiative');
+            this.actor.system.initiative = !this.token?.hasCondition(Condition.SHOCKED) ? iniSkill?.EML : 5;
+            this.actor.system.initiative += iniSkill?.SB.value / 10;
+            if (mark) iniSkill?.item.update({'system.improveFlag': iniSkill.improveFlag + 1});
         }
+
+        console.info(`HM3 | CombatantHM3.getInitiativeRoll: ${truncate(performance.now() - start)} ms`);
 
         return super.getInitiativeRoll(formula);
     }
